cec0b17907d739cca4b0d8fc40e843eb4b11b918
Julian Rojas <julian.rojas87@gmail.com>
2020-11-01 00:35:26 +0100

    Provide starting position to run experiment

--- lc-analytics/run-eval.sh
+++ lc-analytics/run-eval.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+i=0
+
 for dir in /groups/ilabt-imec-be/openplanner/ptn-evaluation/fragmentations/*/; do # list directories
-    dir=${dir%*/}      # remove the trailing "/"
-    ptn=${dir##*/}    # print everything after the final "/"
-    node evaluation.js $ptn $1 5
+    ((i=i+1))
+    if [[ i -gt $2 ]]
+    then
+        dir=${dir%*/}      # remove the trailing "/"
+        ptn=${dir##*/}    # print everything after the final "/"
+        node evaluation.js $ptn $1 1
+    fi
 done
 
