9ae323781a118f672a20514a7cb0a1fa05b24ee2
jrojasme <jrojasme@localhost>
2021-06-17 18:05:11 -0600

    Add timestamp and remove percentage from CPU value

--- scalability-test/server/recordstats.sh
+++ scalability-test/server/recordstats.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Name,%CPU"
+echo timestamp,%CPU
 while true; do
-    docker stats --no-stream --format "{{.Name}},{{.CPUPerc}}"
+    CPU=$(docker stats --no-stream --format "{{.CPUPerc}}")
+    DATE=`date +"%Y-%m-%dT%H:%M:%S"`
+    echo $DATE,${CPU%?}
 done
