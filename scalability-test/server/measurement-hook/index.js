46f6d327b13b2af7f9094e362f08128acdab3fbe
Julian Rojas <julian.rojas87@gmail.com>
2021-06-21 23:33:36 +0200

    Restart docker container when test ends to avoid lagging queries

--- scalability-test/server/measurement-hook/index.js
+++ scalability-test/server/measurement-hook/index.js
@@ -1,19 +1,26 @@
 const http = require('http');
 const { URL } = require('url');
-const { exec } = require('child_process');
+const child_process = require('child_process');
 
-http.createServer((req, res) => {
+const exec = util.promisify(child_process.exec);
+
+http.createServer(async (req, res) => {
     const urlObj = new URL(req.url, `http://${req.headers.host}`);
     const command = urlObj.searchParams.get('command');
     const operator = urlObj.searchParams.get('operator');
     const concurrency = urlObj.searchParams.get('concurrency');
 
     if (command === 'start') {
-        exec(`sudo ../recordstats.sh > ../../results/${operator}/${concurrency}.csv`);
+        await exec(`sudo ../recordstats.sh > ../../results/${operator}/${concurrency}.csv`);
     }
 
     if (command === 'stop') {
-        exec(`sudo pkill -f recordstat`);
+        console.log('Stopping recording script...');
+        await exec('sudo pkill -f recordstat');
+        console.log('Stopping docker container...');
+        await exec('sudo docker rm $(sudo docker ps -a -q)');
+        console.log('Restarting docker container...');
+        await exec('sudo docker run -p 8080:8080 --env-file=../conf.env --cpus=1 otp');
     }
 
     res.statusCode = 200
