8fe3bf55acc118868ae672122be4f32fbf6daf4a
Julian Rojas <julian.rojas87@gmail.com>
2021-06-21 23:03:55 +0200

    Set query timeout to 10s

--- scalability-test/client/otp-scalability.js
+++ scalability-test/client/otp-scalability.js
@@ -80,7 +80,7 @@ async function run() {
             workers: workers[i],
             pipelining: 1,
             amount: concurrencies[i] * iterations * queries.length, // repeat query set {iterations} times per client
-            timeout: 60, // 1 minute timeout for every query
+            timeout: 10, // 10 seconds timeout for every query
             requests: reqs
         });
         console.log(`----------------RESULTS FOR LOAD TEST C=${concurrencies[i]}-----------------`);
