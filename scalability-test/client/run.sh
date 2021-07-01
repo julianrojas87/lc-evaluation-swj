20ffe41335c54c602f9c1f0485a2e15fd19a736c
Julian Rojas <julian.rojas87@gmail.com>
2021-07-01 14:42:31 +0200

    Add more memory to LC client benchmark script

--- scalability-test/client/run.sh
+++ scalability-test/client/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-node ${SERVER}-scalability.js ${SERVER} ${SERVER_URI} ${SERVER_PORT} ${OPERATOR} \
+node --max-old-space-size=4096 ${SERVER}-scalability.js ${SERVER} ${SERVER_URI} ${SERVER_PORT} ${OPERATOR} \
     ${ITERATIONS} ${SUBSET} ${CONCURRENCIES} ${WORKERS} ${LOG} ${RECORD} ${AUTOCANNON}
