e87eb1452194fdfdc951e371277abb6e48ad3c28
Julian Rojas <julian.rojas87@gmail.com>
2021-07-13 14:31:19 +0200

    Adjust running script

--- lc-analytics/run-eval.sh
+++ lc-analytics/run-eval.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-i=0
-
 for dir in /users/jrojasme/lc-data/$1/datasets/*/; do # list directories
-    ((i=i+1))
-    if [[ i -gt $2 ]]
-    then
-        dir=${dir%*/}      # remove the trailing "/"
-        ptn=${dir##*/}    # print everything after the final "/"
-        node --max-old-space-size=8092 scripts/evaluation.js $ptn $1 1
-    fi
+    frag=${dir%*/}      # remove the trailing "/"
+    node --max-old-space-size=12288 scripts/evaluation.js $1 $frag $2
 done
 
