e0cb088cc1ac2aa66548af8953291750c1f0d2fa
Julian Rojas <julian.rojas87@gmail.com>
2021-07-13 14:26:46 +0200

    Fix script issue

--- lc-analytics/reorganize.sh
+++ lc-analytics/reorganize.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-FRAGS=('10' '50' '100' '300' '500' '1000' '3000' '5000' '10000' '20000' '50000')
+FRAGS=('10' '50' '100' '300' '500' '1000' '3000' '5000' '10000' '20000' '30000')
 
 mkdir $1
 mkdir $1/datasets $1/linked_pages $1/stops
 cp min/datasets/$1/* $1/datasets/min
-cp -r min/linked_pages/$1/* $1/linked_pages/min
+cp -r min/linked_pages/$1/ $1/linked_pages/min/
 
 for frag in ${FRAGS[*]}
   do
