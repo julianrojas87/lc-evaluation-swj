f94fe2473e70d8655a5c4ee8a83a9f37c500b1a0
Julian Rojas <julian.rojas87@gmail.com>
2021-07-13 14:44:04 +0200

    Correct path

--- lc-analytics/run-eval.sh
+++ lc-analytics/run-eval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for dir in /users/jrojasme/lc-data/$1/datasets/*/; do # list directories
+for dir in /groups/ilabt-imec-be/openplanner/ptn-evaluation/lc-data-network/$1/datasets/*/; do # list directories
     dir=${dir%*/}      # remove the trailing "/"
     frag=${dir##*/}    # print everything after the final "/"
     node --max-old-space-size=12288 scripts/evaluation.js $1 $frag $2
