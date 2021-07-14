ce46f8931a0f770940fc86a3f9dfadfda80bd50a
Julian Rojas <julian.rojas87@gmail.com>
2021-07-15 00:59:59 +0200

    Update fig 7

--- plots/fig7.py
+++ plots/fig7.py
@@ -6,31 +6,34 @@ def autolabel(rects):
         width = rect.get_width()
         plt.text(1.15*rect.get_width(), rect.get_y()+0.5*rect.get_height(),
                  '%d' % int(width),
-                 ha='center', va='center')
+                 ha='center', va='center', fontsize=14)
 
 
 networks = [
-    "Netherlands-Waterbus (100)", "Kobe-Subway (50)", "Sydney-Trainlink (100)", "San Francisco-BART (100)",
-    "Thailand-Greenbus (100)", "Spain-RENFE (1000)", "Germany-DB (300)", "Auckland-Waiheke (100)",
-    "Belgium-NMBS (1000)", "London-Tube (1000)", "Nairobi-SACCO (3000)", "EU-Flixbus (3000)",
-    "France-SNCF (3000)", "Amsterdam-GVB (1000)", "Brussels-STIB (1000)", "New York-MTABC (300)",
-    "Madrid-CRTM (1000)", "Helsinki-HSL (3000)", "New Zealand-Bus (300)", "Wallonia-TEC (1207)",
-    "Chicago-CTA (3000)", "Flanders-De Lijn (3000)"
+    "Netherlands-Waterbus (100)", "Thailand-Greenbus (1000)", "Kobe-Subway (100)",
+    "Sydney-Trainlink (500)", "San Francisco-BART (300)", "Spain-RENFE (5000)", 
+    "Germany-DB (1000)", "Auckland-Waiheke (300)", "Belgium-NMBS (1000)", 
+    "London-Tube (1000)", "Nairobi-SACCO (3000)", "Amsterdam-GVB (500)",
+    "EU-Flixbus (3000)", "France-SNCF (1000)", "Brussels-STIB (300)", 
+    "New York-MTABC (500)", "Madrid-CRTM (1000)", "Helsinki-HSL (877)", 
+    "New Zealand-Bus (500)", "Chicago-CTA (3000)", 
+    "Flanders-De Lijn (1861)", "Wallonia-TEC (1207)"
 ]
 
 performance = [
-    44, 63, 74, 79, 82, 214, 278, 524, 831, 910,
-    1563, 1997, 2207, 2715, 6883, 8213, 8634, 13494,
-    28706, 29718, 34507, 38461
+    27, 36.27, 37, 45.81, 49.84, 154.23, 182.15, 283.67, 760.62, 942.02, 990.84,
+    1885.51, 2220.44, 2295.44, 4816.93, 5319.87, 7761, 12004.97, 16394.7, 18332.73,
+    34130.91, 36311.72
 ]
 
 x_pos = [i for i, _ in enumerate(networks)]
 
 rects = plt.barh(x_pos, performance, color='green')
 
-plt.ylabel("Public transport network (connections/fragment)", fontsize=14)
-plt.xlabel("(90th percentile) Query response time (ms)", fontsize=14)
-plt.yticks(x_pos, networks, fontsize=12)
+plt.ylabel("Public transport network (connections/fragment)", fontsize=18)
+plt.xlabel("Average query response time (ms)", fontsize=18)
+plt.yticks(x_pos, networks, fontsize=14)
+plt.xticks(fontsize=14)
 plt.xscale('log')
 #plt.gca().invert_yaxis()
 autolabel(rects)
