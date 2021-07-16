22822110e63bf291416ac893358815c2b1140ec1
Julian Rojas <julian.rojas87@gmail.com>
2021-07-16 18:29:51 +0200

    Adjust record stats to record NGINX too

--- scalability-test/server/recordstats.sh
+++ scalability-test/server/recordstats.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo timestamp,%CPU
+echo timestamp,name,%CPU,%Mem
 while true; do
-    CPU=$(docker stats --no-stream --format "{{.CPUPerc}}")
+    LCS_STATS=$(docker stats --no-stream --format "{{.Name}},{{.CPUPerc}},{{.MemPerc}}" server_lc-server_1)
+    NGINX_STATS=$(docker stats --no-stream --format "{{.Name}},{{.CPUPerc}},{{.MemPerc}}" server_nginx_1)
     DATE=`date +"%Y-%m-%dT%H:%M:%S"`
-    echo $DATE,${CPU%?}
+    echo $DATE,${LCS_STATS//%}
+    echo $DATE,${NGINX_STATS//%}
 done
