ddc859e6038af4035f5ca3f408fb43f1dd70e04f
Julian Rojas <julian.rojas87@gmail.com>
2021-07-22 16:31:40 +0200

    Add figure 10

--- plots/fig10.py
+++ plots/fig10.py
@@ -1,100 +1,76 @@
 import matplotlib
 from matplotlib import pyplot as plt
-import csv
+import json
 
-# Function to calculate Y-axis (CPU) values of a transit network
-def get_cpu_usage(ptn, server):
-    root_path = "../scalability-test/server/results/" + ptn + "/" + server + "/"
-    x = [1, 2, 5, 10, 20, 50]
+# Function to calculate Y-axis (average response time) values of a transit network
+def get_response_time(ptn, server):
+    root_path = "../scalability-test/client/results/" + ptn + "/report-" + server + ".json"
+    cc = [1, 2, 5, 10, 20, 50]
+    x = []
     y = []
+    # Opening JSON file
+    data = json.load(open(root_path))
 
-    for i in x:
-        with open(root_path + str(i) + ".csv", 'r') as csvfile:
-            data = list(csv.reader(csvfile, delimiter=','))
-            cpu = 0
-            for index, d in enumerate(data):
-                if(index > 0):
-                    cpu += float(d[2])
-            cpu = cpu / (len(data) - 1)
-            y.append(cpu)
-
-    return x, y
-
-# Function to calculate the average CPU usage of NGINX for a set of networks
-def get_nginx_usage(networks):
-    root_path = "../scalability-test/server/results/"
-    x = [1, 2, 5, 10, 20, 50]
-    y = []
-
-    for i in x:
-        cpu = 0
-        count = 0
-        for net in networks:
-            with open(root_path + net + "/lc_cache/" + str(i) + ".csv", "r") as csvfile:
-                data = list(csv.reader(csvfile, delimiter=','))
-                for index, d in enumerate(data):
-                    if(index > 0 and d[1] == 'server_nginx_1'):
-                        cpu += float(d[2])
-                        count += 1
-        cpu = cpu / count
-        y.append(cpu)
-
+    for i, d in enumerate(data):
+        if(i < 6):
+            if(server == 'otp'):
+                if(d['latency']['average'] > 0):
+                    x.append(cc[i])
+                    y.append(d['latency']['average'])
+            else:
+                if(d['averageResponseTime'] > 0):
+                    x.append(cc[i])
+                    y.append(d['averageResponseTime'])
     return x, y
 
 # Get X and Y axises for each public transport network
-amsterdam_gvb_otp_x, amsterdam_gvb_otp_y = get_cpu_usage("amsterdam-gvb", "otp")
-amsterdam_gvb_lc_x, amsterdam_gvb_lc_y = get_cpu_usage("amsterdam-gvb", "lc_cache")
+amsterdam_gvb_otp_x, amsterdam_gvb_otp_y = get_response_time("amsterdam-gvb", "otp")
+amsterdam_gvb_lc_x, amsterdam_gvb_lc_y = get_response_time("amsterdam-gvb", "lc_cache")
 
-chicago_cta_otp_x, chicago_cta_otp_y = get_cpu_usage("chicago-cta", "otp")
-chicago_cta_lc_x, chicago_cta_lc_y = get_cpu_usage("chicago-cta", "lc_cache")
+chicago_cta_otp_x, chicago_cta_otp_y = get_response_time("chicago-cta", "otp")
+chicago_cta_lc_x, chicago_cta_lc_y = get_response_time("chicago-cta", "lc_cache")
 
-delijn_otp_x, delijn_otp_y = get_cpu_usage("delijn", "otp")
-delijn_lc_x, delijn_lc_y = get_cpu_usage("delijn", "lc_cache")
+delijn_otp_x, delijn_otp_y = get_response_time("delijn", "otp")
+delijn_lc_x, delijn_lc_y = get_response_time("delijn", "lc_cache")
 
-helsinki_hsl_otp_x, helsinki_hsl_otp_y = get_cpu_usage("helsinki-hsl", "otp")
-helsinki_hsl_lc_x, helsinki_hsl_lc_y = get_cpu_usage("helsinki-hsl", "lc_cache")
+helsinki_hsl_otp_x, helsinki_hsl_otp_y = get_response_time("helsinki-hsl", "otp")
+helsinki_hsl_lc_x, helsinki_hsl_lc_y = get_response_time("helsinki-hsl", "lc_cache")
 
-kobe_subway_otp_x, kobe_subway_otp_y = get_cpu_usage("kobe-subway", "otp")
-kobe_subway_lc_x, kobe_subway_lc_y = get_cpu_usage("kobe-subway", "lc_cache")
+kobe_subway_otp_x, kobe_subway_otp_y = get_response_time("kobe-subway", "otp")
+kobe_subway_lc_x, kobe_subway_lc_y = get_response_time("kobe-subway", "lc_cache")
 
-london_tube_otp_x, london_tube_otp_y = get_cpu_usage("london-tube", "otp")
-london_tube_lc_x, london_tube_lc_y = get_cpu_usage("london-tube", "lc_cache")
+london_tube_otp_x, london_tube_otp_y = get_response_time("london-tube", "otp")
+london_tube_lc_x, london_tube_lc_y = get_response_time("london-tube", "lc_cache")
 
-madrid_bus_otp_x, madrid_bus_otp_y = get_cpu_usage("madrid-bus", "otp")
-madrid_bus_lc_x, madrid_bus_lc_y = get_cpu_usage("madrid-bus", "lc_cache")
+madrid_bus_otp_x, madrid_bus_otp_y = get_response_time("madrid-bus", "otp")
+madrid_bus_lc_x, madrid_bus_lc_y = get_response_time("madrid-bus", "lc_cache")
 
-nairobi_sacco_otp_x, nairobi_sacco_otp_y = get_cpu_usage("nairobi-sacco", "otp")
-nairobi_sacco_lc_x, nairobi_sacco_lc_y = get_cpu_usage("nairobi-sacco", "lc_cache")
+nairobi_sacco_otp_x, nairobi_sacco_otp_y = get_response_time("nairobi-sacco", "otp")
+nairobi_sacco_lc_x, nairobi_sacco_lc_y = get_response_time("nairobi-sacco", "lc_cache")
 
-new_york_mtabc_otp_x, new_york_mtabc_otp_y = get_cpu_usage("new-york-mtabc", "otp")
-new_york_mtabc_lc_x, new_york_mtabc_lc_y = get_cpu_usage("new-york-mtabc", "lc_cache")
+new_york_mtabc_otp_x, new_york_mtabc_otp_y = get_response_time("new-york-mtabc", "otp")
+new_york_mtabc_lc_x, new_york_mtabc_lc_y = get_response_time("new-york-mtabc", "lc_cache")
 
-nl_waterbus_otp_x, nl_waterbus_otp_y = get_cpu_usage("nl-waterbus", "otp")
-nl_waterbus_lc_x, nl_waterbus_lc_y = get_cpu_usage("nl-waterbus", "lc_cache")
+nl_waterbus_otp_x, nl_waterbus_otp_y = get_response_time("nl-waterbus", "otp")
+nl_waterbus_lc_x, nl_waterbus_lc_y = get_response_time("nl-waterbus", "lc_cache")
 
-nmbs_otp_x, nmbs_otp_y = get_cpu_usage("nmbs", "otp")
-nmbs_lc_x, nmbs_lc_y = get_cpu_usage("nmbs", "lc_cache")
+nmbs_otp_x, nmbs_otp_y = get_response_time("nmbs", "otp")
+nmbs_lc_x, nmbs_lc_y = get_response_time("nmbs", "lc_cache")
 
-sf_bart_otp_x, sf_bart_otp_y = get_cpu_usage("sf-bart", "otp")
-sf_bart_lc_x, sf_bart_lc_y = get_cpu_usage("sf-bart", "lc_cache")
+sf_bart_otp_x, sf_bart_otp_y = get_response_time("sf-bart", "otp")
+sf_bart_lc_x, sf_bart_lc_y = get_response_time("sf-bart", "lc_cache")
 
-stib_otp_x, stib_otp_y = get_cpu_usage("stib", "otp")
-stib_lc_x, stib_lc_y = get_cpu_usage("stib", "lc_cache")
+stib_otp_x, stib_otp_y = get_response_time("stib", "otp")
+stib_lc_x, stib_lc_y = get_response_time("stib", "lc_cache")
 
-sydney_trainlink_otp_x, sydney_trainlink_otp_y = get_cpu_usage("sydney-trainlink", "otp")
-sydney_trainlink_lc_x, sydney_trainlink_lc_y = get_cpu_usage("sydney-trainlink", "lc_cache")
+sydney_trainlink_otp_x, sydney_trainlink_otp_y = get_response_time("sydney-trainlink", "otp")
+sydney_trainlink_lc_x, sydney_trainlink_lc_y = get_response_time("sydney-trainlink", "lc_cache")
 
-tec_otp_x, tec_otp_y = get_cpu_usage("tec", "otp")
-tec_lc_x, tec_lc_y = get_cpu_usage("tec", "lc_cache")
+tec_otp_x, tec_otp_y = get_response_time("tec", "otp")
+tec_lc_x, tec_lc_y = get_response_time("tec", "lc_cache")
 
-thailand_greenbus_otp_x, thailand_greenbus_otp_y = get_cpu_usage("thailand-greenbus", "otp")
-thailand_greenbus_lc_x, thailand_greenbus_lc_y = get_cpu_usage("thailand-greenbus", "lc_cache")
-
-# Calculate NGINX average CPU usage per plot
-#nginx1_x, nginx1_y = get_nginx_usage(['sydney-trainlink', 'thailand-greenbus', 'kobe-subway'])
-#nginx2_x, nginx2_y = get_nginx_usage(['nairobi-sacco', 'sf-bart', 'amsterdam-gvb', 'nmbs'])
-#nginx3_x, nginx3_y = get_nginx_usage(['stib', 'london-tube', 'new-york-mtabc', 'madrid-bus'])
-#nginx4_x, nginx4_y = get_nginx_usage(['tec', 'delijn', 'chicago-cta', 'helsinki-hsl'])
+thailand_greenbus_otp_x, thailand_greenbus_otp_y = get_response_time("thailand-greenbus", "otp")
+thailand_greenbus_lc_x, thailand_greenbus_lc_y = get_response_time("thailand-greenbus", "lc_cache")
 
 # Create subplots
 plt.rcParams.update({'font.size': 12})
@@ -106,11 +82,12 @@ ax1.plot(sydney_trainlink_otp_x, sydney_trainlink_otp_y, marker='o', markersize=
 ax1.plot(thailand_greenbus_otp_x, thailand_greenbus_otp_y, marker='o', markersize=3.5, label="Thailand-Greenbus", linewidth=lw)
 ax1.plot(kobe_subway_otp_x, kobe_subway_otp_y, marker='o', markersize=3.5, label="Kobe-Subway", linewidth=lw)
 ax1.set_title("OpenTripPlanner Server")
-ax1.set_ylabel("cpu use (%)")
+ax1.set_ylabel("average response time (ms)")
 ax1.set_xscale("log")
+ax1.set_yscale("log")
 ax1.set_xticks([1, 2, 5, 10, 20, 50])
 ax1.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-ax1.set_yticks([0, 20, 40, 60, 80, 100])
+ax1.set_yticks([20, 100, 200, 500, 1000, 2000])
 ax1.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax1.spines["right"].set_visible(False)
 ax1.spines["top"].set_visible(False)
@@ -121,11 +98,12 @@ ax3.plot(nairobi_sacco_otp_x, nairobi_sacco_otp_y, marker='o', markersize=3.5, l
 ax3.plot(sf_bart_otp_x, sf_bart_otp_y, marker='o', markersize=3.5, label="San Francisco-BART", linewidth=lw)
 ax3.plot(amsterdam_gvb_otp_x, amsterdam_gvb_otp_y, marker='o', markersize=3.5, label="Amsterdam-GVB", linewidth=lw)
 ax3.plot(nmbs_otp_x, nmbs_otp_y, marker='o', markersize=3.5, label="Belgium-NMBS", linewidth=lw)
-ax3.set_ylabel("cpu use (%)")
+ax3.set_ylabel("average response time (ms)")
 ax3.set_xscale("log")
+ax3.set_yscale("log")
 ax3.set_xticks([1, 2, 5, 10, 20, 50])
 ax3.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-ax3.set_yticks([0, 20, 40, 60, 80, 100])
+ax3.set_yticks([50, 100, 500, 1000, 3000, 7000])
 ax3.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax3.spines["right"].set_visible(False)
 ax3.spines["top"].set_visible(False)
@@ -136,11 +114,12 @@ ax5.plot(stib_otp_x, stib_otp_y, marker='o', markersize=3.5, label="Brussels-STI
 ax5.plot(london_tube_otp_x, london_tube_otp_y, marker='o', markersize=3.5, label="London-Tube", linewidth=lw)
 ax5.plot(new_york_mtabc_otp_x, new_york_mtabc_otp_y, marker='o', markersize=3.5, label="New York-MTABC", linewidth=lw)
 ax5.plot(madrid_bus_otp_x, madrid_bus_otp_y, marker='o', markersize=3.5, label="Madrid-CRTM", linewidth=lw)
-ax5.set_ylabel("cpu use (%)")
+ax5.set_ylabel("average response time (ms)")
 ax5.set_xscale("log")
+ax5.set_yscale("log")
 ax5.set_xticks([1, 2, 5, 10, 20, 50])
 ax5.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-ax5.set_yticks([0, 20, 40, 60, 80, 100])
+ax5.set_yticks([200, 500, 1500, 3000, 8000, 20000])
 ax5.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax5.spines["right"].set_visible(False)
 ax5.spines["top"].set_visible(False)
@@ -152,16 +131,16 @@ ax7.plot(delijn_otp_x, delijn_otp_y, marker='o', markersize=3.5, label="Flanders
 ax7.plot(helsinki_hsl_otp_x, helsinki_hsl_otp_y, marker='o', markersize=3.5, label="Helsinki-HSL", linewidth=lw)
 ax7.plot(chicago_cta_otp_x, chicago_cta_otp_y, marker='o', markersize=3.5, label="Chicago-CTA", linewidth=lw)
 ax7.set_xlabel("concurrent clients")
-ax7.set_ylabel("cpu use (%)")
+ax7.set_ylabel("average response time (ms)")
 ax7.set_xscale("log")
+ax7.set_yscale("log")
 ax7.set_xticks([1, 2, 5, 10, 20, 50])
 ax7.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-ax7.set_yticks([0, 20, 40, 60, 80, 100])
+ax7.set_yticks([500, 1500, 3000, 10000, 30000, 60000])
 ax7.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax7.spines["right"].set_visible(False)
 ax7.spines["top"].set_visible(False)
 ax7.grid(alpha=0.3)
-
 
 ax2.plot(nl_waterbus_lc_x, nl_waterbus_lc_y, marker='o', markersize=3.5, label="Netherlands-Waterbus", linewidth=lw)
 ax2.plot(sydney_trainlink_lc_x, sydney_trainlink_lc_y, marker='o', markersize=3.5, label="Sydney-Trainlink", linewidth=lw)
@@ -169,42 +148,42 @@ ax2.plot(thailand_greenbus_lc_x, thailand_greenbus_lc_y, marker='o', markersize=
 ax2.plot(kobe_subway_lc_x, kobe_subway_lc_y, marker='o', markersize=3.5, label="Kobe-Subway", linewidth=lw)
 ax2.set_title("Linked Connections Server")
 ax2.set_xscale("log")
+ax2.set_yscale("log")
 ax2.set_xticks([1, 2, 5, 10, 20, 50])
 ax2.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-ax2.set_yticks([0, 20, 40, 60, 80, 100])
+ax2.set_yticks([20, 100, 200, 500, 1000, 2000])
 ax2.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax2.spines["right"].set_visible(False)
 ax2.spines["top"].set_visible(False)
 ax2.grid(alpha=0.3)
-
 
 ax4.plot(nairobi_sacco_lc_x, nairobi_sacco_lc_y, marker='o', markersize=3.5, label="Nairobi-SACCO", linewidth=lw)
 ax4.plot(sf_bart_lc_x, sf_bart_lc_y, marker='o', markersize=3.5, label="San Francisco-BART", linewidth=lw)
 ax4.plot(amsterdam_gvb_lc_x, amsterdam_gvb_lc_y, marker='o', markersize=3.5, label="Amsterdam-GVB", linewidth=lw)
 ax4.plot(nmbs_lc_x, nmbs_lc_y, marker='o', markersize=3.5, label="Belgium-NMBS", linewidth=lw)
 ax4.set_xscale("log")
+ax4.set_yscale("log")
 ax4.set_xticks([1, 2, 5, 10, 20, 50])
 ax4.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-ax4.set_yticks([0, 20, 40, 60, 80, 100])
+ax4.set_yticks([50, 100, 500, 1000, 3000, 7000])
 ax4.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax4.spines["right"].set_visible(False)
 ax4.spines["top"].set_visible(False)
 ax4.grid(alpha=0.3)
-
 
 ax6.plot(stib_lc_x, stib_lc_y, marker='o', markersize=3.5, label="Brussels-STIB", linewidth=lw)
 ax6.plot(london_tube_lc_x, london_tube_lc_y, marker='o', markersize=3.5, label="London-Tube", linewidth=lw)
 ax6.plot(new_york_mtabc_lc_x, new_york_mtabc_lc_y, marker='o', markersize=3.5, label="New York-MTABC", linewidth=lw)
 ax6.plot(madrid_bus_lc_x, madrid_bus_lc_y, marker='o', markersize=3.5, label="Madrid-CRTM", linewidth=lw)
 ax6.set_xscale("log")
+ax6.set_yscale("log")
 ax6.set_xticks([1, 2, 5, 10, 20, 50])
 ax6.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-ax6.set_yticks([0, 20, 40, 60, 80, 100])
+ax6.set_yticks([200, 500, 1500, 3000, 8000, 20000])
 ax6.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax6.spines["right"].set_visible(False)
 ax6.spines["top"].set_visible(False)
 ax6.grid(alpha=0.3)
-
 
 ax8.plot(tec_lc_x, tec_lc_y, marker='o', markersize=3.5, label="Wallonia-TEC", linewidth=lw)
 ax8.plot(delijn_lc_x, delijn_lc_y, marker='o', markersize=3.5, label="Flanders-De Lijn", linewidth=lw)
@@ -212,24 +191,24 @@ ax8.plot(helsinki_hsl_lc_x, helsinki_hsl_lc_y, marker='o', markersize=3.5, label
 ax8.plot(chicago_cta_lc_x, chicago_cta_lc_y, marker='o', markersize=3.5, label="Chicago-CTA", linewidth=lw)
 ax8.set_xlabel("concurrent clients")
 ax8.set_xscale("log")
+ax8.set_yscale("log")
 ax8.set_xticks([1, 2, 5, 10, 20, 50])
 ax8.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-ax8.set_yticks([0, 20, 40, 60, 80, 100])
+ax8.set_yticks([500, 1500, 3000, 10000, 30000, 60000])
 ax8.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax8.spines["right"].set_visible(False)
 ax8.spines["top"].set_visible(False)
 ax8.grid(alpha=0.3)
-
 
 handles1, labels1 = ax1.get_legend_handles_labels()
 handles3, labels3 = ax3.get_legend_handles_labels()
 handles5, labels5 = ax5.get_legend_handles_labels()
 handles7, labels7 = ax7.get_legend_handles_labels()
 
-fig.legend(handles1, labels1, loc="upper right", ncol=1, handlelength=3)
-fig.legend(handles3, labels3, loc="center left", ncol=1, handlelength=3)
-fig.legend(handles5, labels5, loc="center right", ncol=1, handlelength=3)
-fig.legend(handles7, labels7, loc="lower right", ncol=1, handlelength=3)
+fig.legend(handles1, labels1, loc="upper right", ncol=1)
+fig.legend(handles3, labels3, loc="center left", ncol=1)
+fig.legend(handles5, labels5, loc="center right", ncol=1)
+fig.legend(handles7, labels7, loc="lower right", ncol=1)
 
 #fig.set_size_inches(25, 20)
 #plt.savefig("/home/julian/Desktop/test.svg")
